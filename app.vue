9f9ee05fd725279ab2ae83fda4ee14d4a9536d30
rasyidly <rasyid@sekeco.id>
2023-05-23 21:55:29 +0700

    Little typo

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup>
-const title = 'Islami(c) Fest 2023 - Ramah Keberagaman & Ramah Anak';
+const title = 'Islamifest 2023 - Ramah Keberagaman & Ramah Anak';
 const description = 'Ruang perjumpaan para pegiat media Islam, content creator, dan influencer yang fokus pada kampanye Islam ramah dan moderat di ruang digital';
 
 useHead({
